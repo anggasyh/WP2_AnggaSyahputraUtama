069fcf7c27d19f3a040d86964fafe719ccde060b
anggasyh <anggasyh21@gmail.com>
2023-04-06 11:46:42 +0700

    Mengerjakan Tugas Pertemuan 3 dan 4

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -4,9 +4,9 @@ class Contoh1 extends CI_Controller
   public function index()
  {
    echo "<h1>Perkenalkan</h1>";
-   echo"Nama saya Rangga Faishal Syahputra
-   Saya tingga di daerah Depok
+   echo"Nama saya Angga Syahputra
+   Saya tingga di daerah Bogor
    olahraga yang saya sukai adalah
-   Golf";
+   Basket";
  }
 }
--- application/views/v_about.php
+++ application/views/v_about.php
@@ -2,23 +2,23 @@
     <h1><?php echo $judul ?></h1>
     <h4>Nama</h4>
     <ul type="disc">
-        <li>Nama Depan : Rangga</li>
-        <li>Nama Belakang : Faishal</li>
+        <li>Nama Depan : Angga</li>
+        <li>Nama Belakang : Syahputra</li>
     </ul>
     <br>
     <h4>Alamat</h4>
     <ul type="none">
-        <li>Depok</li>
+        <li>Bogor Kota</li>
     </ul>
 
     <h4>Tempat Lahir</h4>
     <ul type="none">
-        <li>Depok</li>
+        <li>Sabang</li>
     </ul>
 
     <h4>Olah Raga Favorit</h4>
     <ul type="square">
-        <li>Golf</li>
-        <li>Bowling</li>
+        <li>Basket</li>
+        <li>Sepakbola</li>
     </ul>
 </section>
