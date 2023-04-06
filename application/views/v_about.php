d2577212a3e4237de8e682f0188146483e5b713b
sgnarr <ranggafs09@gmail.com>
2023-04-06 11:33:07 +0700

    Tugas Pertemuan 3&4

--- application/views/v_about.php
+++ application/views/v_about.php
@@ -2,23 +2,23 @@
     <h1><?php echo $judul ?></h1>
     <h4>Nama</h4>
     <ul type="disc">
-        <li>Nama Depan : Angga</li>
-        <li>Nama Belakang : Syahputra</li>
+        <li>Nama Depan : Rangga</li>
+        <li>Nama Belakang : Faishal</li>
     </ul>
     <br>
     <h4>Alamat</h4>
     <ul type="none">
-        <li>Bogor Kota</li>
+        <li>Depok</li>
     </ul>
 
     <h4>Tempat Lahir</h4>
     <ul type="none">
-        <li>Sabang</li>
+        <li>Depok</li>
     </ul>
 
     <h4>Olah Raga Favorit</h4>
     <ul type="square">
-        <li>Basket</li>
-        <li>Sepakbola</li>
+        <li>Golf</li>
+        <li>Bowling</li>
     </ul>
 </section>
